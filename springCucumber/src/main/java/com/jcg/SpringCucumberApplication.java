a24a04c0252756e455b122a6dd4686b24c7a3592
Rajagopal <psraja@thoughtworks.com>
2019-05-05 12:52:28 +0530

    Check in cucumber with spring boot

---
+++ springCucumber/src/main/java/com/jcg/SpringCucumberApplication.java
@@ -0,0 +1,11 @@
+package com.jcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCucumberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCucumberApplication.class, args);
+    }
+}
---
+++ springCucumber/src/test/java/com/jcg/cucumber/SpringIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.jcg.cucumber;
+
+import com.jcg.SpringCucumberApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(
+        classes = SpringCucumberApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SpringIntegrationTest {
+
+    protected RestTemplate restTemplate = new RestTemplate();
+
+    protected final String DEFAULT_URL = "http://localhost:8082/";
+
+
+}
