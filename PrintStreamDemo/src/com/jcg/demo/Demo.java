1aad9cebb01605f0eb7d3c7c599001ff90e51bb5
Raja <rajagopal.sarathi@gmail.com>
2020-01-25 15:39:34 +0530

    Printstream Demo

---
+++ PrintStreamDemo/src/com/jcg/demo/Demo.java
@@ -0,0 +1,29 @@
+package com.jcg.demo;
+
+import java.io.*;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        File file = new File("test.txt");
+        PrintStream printStream = new PrintStream(file);
+        printStream.println("test");
+
+        FileOutputStream fileOutputStream = new FileOutputStream("test.txt");
+        printStream = new PrintStream(fileOutputStream, true, "UTF-8");
+        printStream.println("test1");
+
+        printStream = new PrintStream("test.txt");
+        printStream.println("hi");
+        printStream.print((Object) null);
+        printStream.append("hello", 0, 3);
+        printStream.printf("%nIt is %d year to %s", 1, "2021");
+        printStream.println();
+        printStream.write("bytes".getBytes());
+        printStream.close();
+        printStream.println("text not written");
+        System.out.println("Error State:" + printStream.checkError());
+
+
+    }
+}
+
