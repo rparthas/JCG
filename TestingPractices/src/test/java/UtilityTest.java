943628d3f35c3c86ca7d011d0170cc0a121a5e04
Raja <rajagopal.sarathi@gmail.com>
2020-08-06 11:51:11 +0530

    Remove parameterized

--- TestingPractices/src/test/java/UtilityTest.java
+++ TestingPractices/src/test/java/UtilityTest.java
@@ -1,7 +1,6 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
